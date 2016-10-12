17e7b7770c7b0e0b61952260b3e88b79c09dd91d
Damir <luketic.damir@gmail.com>
2016-10-12 13:35:16 +0200

    add src to git

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -5,10 +5,10 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RECIPE_ROUTES } from "./recipes/recipes.routes";
 
 const APP_ROUTES: Routes = [
-  {path: '', redirectTo: 'systems', pathMatch: 'full'},
+  {path: '', redirectTo: '/systems', pathMatch: 'full'},
   {path: 'systems', component: RecipesComponent, children: RECIPE_ROUTES},
   {path: 'shopping-list', component: ShoppingListComponent},
-  {path: '**', redirectTo: 'systems', pathMatch: 'full'}
+  {path: '**', redirectTo: '/systems', pathMatch: 'full'}
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
